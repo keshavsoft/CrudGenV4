8367b10e72eb651b0ffa488a29567d4ceedf09b0
keshavsoft <nknnkumar@yahoo.com>
2025-01-30 17:34:01 +0530

    Common working

--- Common/CommonBin/FilesWithData/ActualData/kLowDb/PullData.js
+++ Common/CommonBin/FilesWithData/ActualData/kLowDb/PullData.js
@@ -1,39 +1,5 @@
-import fs from "fs";
-import path from "path";
-import ConfigJson from '../../../../../binV4/Config.json' assert {type: 'json'};
-
 import { StartFunc as StartFuncCommonReadFiles } from "../../../../CommonReadFiles/readAllFiles.js";
 
-let StartFunc = () => {
-    let LocalReturnData = { KTF: false }
-
-    LocalReturnData.KTF = true;
-    LocalReturnData.JsonData = StartFuncCommonReadFiles();
-
-    return LocalReturnData;
-};
-
-let StartFunc1 = () => {
-    let LocalReturnData = { KTF: false }
-
-    let LocalDataPath = `${ConfigJson.jsonConfig.DataPath}/${ConfigJson.jsonConfig.DataPk}`;
-
-    let files = fs.readdirSync(LocalDataPath)
-        .filter(filename => filename.endsWith('.json'))
-        .map(filename => {
-            const data = fs.readFileSync(`${LocalDataPath}/${filename}`, { encoding: 'utf8' });
-            let JsonParseData = JSON.parse(data);
-
-            let LoopInsideObject = {};
-            LoopInsideObject.FileName = path.parse(filename).name;
-            LoopInsideObject.FileData = JsonParseData;
-            return LoopInsideObject;
-        });
-
-    LocalReturnData.KTF = true;
-    LocalReturnData.JsonData = files;
-
-    return LocalReturnData;
-};
+let StartFunc = StartFuncCommonReadFiles;
 
 export { StartFunc };
--- Common/CommonReadFiles/readAllFiles.js
+++ Common/CommonReadFiles/readAllFiles.js
@@ -1,26 +1,11 @@
 import fs from "fs";
-import path from "path";
 import ConfigJson from '../../binV4/Config.json' assert {type: 'json'};
-import { StartFunc as StartFuncReadFileData } from "./readFileData.js";
+import { StartFunc as StartFuncLoopFiles } from "./loopFiles.js";
 
 let StartFunc = () => {
-    let LocalReturnData = { KTF: false }
-
-    let LocalDataPath = `${ConfigJson.jsonConfig.DataPath}/${ConfigJson.jsonConfig.DataPk}`;
-
     let LocalFromAllFiles = LocalFuncReturnAllFiles();
 
-    const files = LocalFromAllFiles.map(filename => {
-        let LoopInsideObject = {};
-        LoopInsideObject.FileName = path.parse(filename).name;
-        LoopInsideObject.FileData = StartFuncReadFileData({ inFilePath: `${LocalDataPath}/${filename}` });
-        return LoopInsideObject;
-    });
-
-    LocalReturnData.KTF = true;
-    LocalReturnData.JsonData = files;
-
-    return LocalReturnData;
+    return StartFuncLoopFiles({ inFilesAsArray: LocalFromAllFiles });
 };
 
 let LocalFuncReturnAllFiles = () => {
@@ -30,29 +15,6 @@ let LocalFuncReturnAllFiles = () => {
         .filter(filename => filename.endsWith('.json'));
 
     return files;
-};
-
-let StartFunc1 = () => {
-    let LocalReturnData = { KTF: false }
-
-    let LocalDataPath = `${ConfigJson.jsonConfig.DataPath}/${ConfigJson.jsonConfig.DataPk}`;
-
-    let files = fs.readdirSync(LocalDataPath)
-        .filter(filename => filename.endsWith('.json'))
-        .map(filename => {
-            const data = fs.readFileSync(`${LocalDataPath}/${filename}`, { encoding: 'utf8' });
-            let JsonParseData = JSON.parse(data);
-
-            let LoopInsideObject = {};
-            LoopInsideObject.FileName = path.parse(filename).name;
-            LoopInsideObject.FileData = JsonParseData;
-            return LoopInsideObject;
-        });
-
-    LocalReturnData.KTF = true;
-    LocalReturnData.JsonData = files;
-
-    return LocalReturnData;
 };
 
 export { StartFunc };
