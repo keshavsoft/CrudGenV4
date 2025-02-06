39909e938dba65dcc2969deb355d4e3a9e5efe1f
pushpadamalanka82 <y*********@gmail.com>
2025-02-06 19:24:27 +0530

    dcdate

--- Custom/Cleaning/Branch/Factory/FromFactory/Completion/QrCode/kLowDb/ReadFileList/All.js
+++ Custom/Cleaning/Branch/Factory/FromFactory/Completion/QrCode/kLowDb/ReadFileList/All.js
@@ -41,7 +41,7 @@ let jFLocalMergeFunc = ({ inQrData, inScandata, inEntryScan, inBranchDC }) => {
             Rate: matchedRecord?.Rate,
 
             VoucherNumber: matchedBranchDC?.pk,
-            DCDate: new Date(matchedBranchDC?.DCDate).toLocaleDateString('en-GB'),
+            DCDate: new Date(loopScan?.DCDate).toLocaleDateString('en-GB'),
 
             QrCodeId: loopScan.QrCodeId,
             BranchName: loopScan?.BranchName,
