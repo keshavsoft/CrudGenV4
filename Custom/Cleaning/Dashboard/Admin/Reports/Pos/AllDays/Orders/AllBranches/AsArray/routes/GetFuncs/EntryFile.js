acc25eff994af5b7519d4429bb9539aa3487d752
pushpadamalanka82 <y*********@gmail.com>
2025-02-07 17:19:40 +0530

    new endpoint

--- Custom/Cleaning/Dashboard/Admin/Reports/Pos/AllDays/Orders/AllBranches/AsArray/routes/GetFuncs/EntryFile.js
+++ Custom/Cleaning/Dashboard/Admin/Reports/Pos/AllDays/Orders/AllBranches/AsArray/routes/GetFuncs/EntryFile.js
@@ -3,9 +3,11 @@ import express from 'express';
 var router = express.Router();
 
 import {
-    GetFuncs
+    GetFuncs, SimpleFuncs, ItemCountFuncs
 } from '../../controllers/GetFuncs/EntryFile.js';
 
 router.get('/', GetFuncs);
+router.get('/Simple', SimpleFuncs);
+router.get('/ItemCount', ItemCountFuncs);
 
 export { router };
