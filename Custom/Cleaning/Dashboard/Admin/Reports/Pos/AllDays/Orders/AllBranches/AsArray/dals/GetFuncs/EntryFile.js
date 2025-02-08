b6398812b7c188a780729d7ca94600611b52b8b0
pushpadamalanka82 <y*********@gmail.com>
2025-02-08 11:52:52 +0530

    new endpoints

--- Custom/Cleaning/Dashboard/Admin/Reports/Pos/AllDays/Orders/AllBranches/AsArray/dals/GetFuncs/EntryFile.js
+++ Custom/Cleaning/Dashboard/Admin/Reports/Pos/AllDays/Orders/AllBranches/AsArray/dals/GetFuncs/EntryFile.js
@@ -1,6 +1,7 @@
 import { StartFunc as StartFuncmergeArray } from '../../kLowDb/ReadFromFile/mergeArray.js';
 import { StartFunc as StartFuncsimple } from '../../kLowDb/ReadFromFile/simple.js';
-
+import { StartFunc as StartFuncsimple } from '../../kLowDb/ReadFromFile/simple.js';
+import { StartFunc as StartFuncisSettled } from '../../kLowDb/ReadFromFile/isettled.js';
 
 let GetAllFuncs = () => {
     return StartFuncmergeArray();
@@ -14,6 +15,18 @@ let ItemCountFuncs = () => {
     return StartFuncmergeArray();
 };
 
+let IsSettledFuncs = () => {
+    return StartFuncisSettled();
+};
+
+let WithSettlementFuncs = () => {
+    return StartFuncmergeArray();
+};
+
+let WithDeliveryFuncs = () => {
+    return StartFuncmergeArray();
+};
+
 export {
-    GetAllFuncs, SimpleFuncs, ItemCountFuncs
+    GetAllFuncs, SimpleFuncs, ItemCountFuncs, IsSettledFuncs, WithSettlementFuncs, WithDeliveryFuncs
 };
--- Custom/Cleaning/Dashboard/Admin/Reports/Pos/AllDays/Orders/AllBranches/AsArray/routes/GetFuncs/EntryFile.js
+++ Custom/Cleaning/Dashboard/Admin/Reports/Pos/AllDays/Orders/AllBranches/AsArray/routes/GetFuncs/EntryFile.js
@@ -3,11 +3,14 @@ import express from 'express';
 var router = express.Router();
 
 import {
-    GetFuncs, SimpleFuncs, ItemCountFuncs
+    GetFuncs, SimpleFuncs, ItemCountFuncs, IsSettledFuncs, WithSettlementFuncs, WithDeliveryFuncs
 } from '../../controllers/GetFuncs/EntryFile.js';
 
 router.get('/', GetFuncs);
 router.get('/Simple', SimpleFuncs);
 router.get('/ItemCount', ItemCountFuncs);
+router.get('/IsSettled', IsSettledFuncs);
+router.get('/WithSettlement,', WithSettlementFuncs);
+router.get('/WithDelivery,', WithDeliveryFuncs);
 
 export { router };
