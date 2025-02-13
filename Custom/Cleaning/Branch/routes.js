a2beda2e9be40dacc04765a2cef5a0e0354c7c7c
Srivasu215 <sri.vasu215@gmail.com>
2025-02-13 15:36:30 +0530

    Reports all qrcodes

--- Custom/Cleaning/Branch/routes.js
+++ Custom/Cleaning/Branch/routes.js
@@ -7,13 +7,14 @@ import { router as Factory } from './Factory/routes.js';
 import { router as Today } from './Today/routes.js';
 import { router as Order} from './Order/routes.js';
 import { router as Dashboard} from './Dashboard/routes.js';
-
+import { router as Reports} from './Reports/routes.js';
 
 router.use('/QrCode', QrCode);
 router.use('/Factory', Factory);
 router.use('/Today', Today);
 router.use('/Order', Order);
 router.use('/Dashboard', Dashboard);
+router.use('/Reports', Reports);
 
 
 export { router };
