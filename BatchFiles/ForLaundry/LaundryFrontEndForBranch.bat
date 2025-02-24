97413a1b3cc04cd8b791d2ffd74f77713d4322d1
pushpadamalanka82 <y*********@gmail.com>
2025-02-24 18:37:54 +0530

    batchfile

--- BatchFiles/ForLaundry/LaundryFrontEndForBranch.bat
+++ BatchFiles/ForLaundry/LaundryFrontEndForBranch.bat
@@ -27,7 +27,7 @@ call npm run Delivery
 call npm run Orders
 call npm run Payments
 call npm run QrCodes
-call npm run DcSend
+call npm run All-Dcs
 
 xcopy .\publicDir ..\CrudGenV4\public\Laundry\Branch /h /i /c /k /e /r /y
 
