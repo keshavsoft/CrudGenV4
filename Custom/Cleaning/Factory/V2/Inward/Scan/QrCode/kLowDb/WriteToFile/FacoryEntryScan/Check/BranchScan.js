bab58423a05ddb254287e74a045f38f77e3b5a10
sharath0036 <ksharathbabu2@gmail.com>
2025-03-03 12:13:35 +0530

    check dc

--- Custom/Cleaning/Factory/V2/Inward/Scan/QrCode/kLowDb/WriteToFile/FacoryEntryScan/Check/BranchScan.js
+++ Custom/Cleaning/Factory/V2/Inward/Scan/QrCode/kLowDb/WriteToFile/FacoryEntryScan/Check/BranchScan.js
@@ -1,13 +1,13 @@
 import { StartFunc as StartFuncCommonFuncs } from '../../../CommonFuncs/BranToFactBScan.js';
+import { StartFunc as BranToFactDC } from '../../../CommonFuncs/BranToFactDC.js';
 
 const StartFunc = ({ inTable, inDc, inQrCodeId }) => {
     let LocalFactoryName = inTable;
     let LocalDc = inDc;
     let LocalQrCodeId = inQrCodeId;
-
     let LocalReturnData = { KTF: false };
-
     const LocalJsonData = StartFuncCommonFuncs();
+    const LocalDcData = BranToFactDC();
 
     let LocalQrCheck = LocalJsonData.find(e => e.QrCodeId == LocalQrCodeId);
 
@@ -16,17 +16,20 @@ const StartFunc = ({ inTable, inDc, inQrCodeId }) => {
         return LocalReturnData;
     };
 
-    let LocalRowNeeded = LocalJsonData.find(e => e.QrCodeId == LocalQrCodeId && e.VoucherRef == LocalDc);
-
-    if (LocalRowNeeded === undefined) {
+    if (parseInt(LocalQrCheck.VoucherRef) !== LocalDc) {
         LocalReturnData.KReason = `Not this Dc :${LocalDc}`
         return LocalReturnData;
     };
 
-    let LocalFactoryCheck = LocalJsonData.find(e => e.DCFactory == LocalFactoryName);
-
-    if (LocalFactoryCheck === undefined) {
+    if (LocalQrCheck.DCFactory !== LocalFactoryName) {
         LocalReturnData.KReason = `Not this Factory :${LocalFactoryName}`
+        return LocalReturnData;
+    };
+
+    let LocaldcCheck = LocalDcData.find(e => e.pk == LocalDc);
+
+    if (LocaldcCheck.SendDc == undefined || LocaldcCheck.SendDc !== true) {
+        LocalReturnData.KReason = `Dc Not Sent :${LocalFactoryName}`
         return LocalReturnData;
     };
 
