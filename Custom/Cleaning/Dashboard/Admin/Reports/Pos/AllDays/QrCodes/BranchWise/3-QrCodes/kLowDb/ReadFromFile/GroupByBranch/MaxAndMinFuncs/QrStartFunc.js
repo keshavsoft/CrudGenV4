0224d2037b5865fd2d4d58bc4a0f78b350aa7a8c
sharath0036 <ksharathbabu2@gmail.com>
2025-03-05 18:43:17 +0530

    small bug

--- Custom/Cleaning/Dashboard/Admin/Reports/Pos/AllDays/QrCodes/BranchWise/3-QrCodes/kLowDb/ReadFromFile/GroupByBranch/MaxAndMinFuncs/QrStartFunc.js
+++ Custom/Cleaning/Dashboard/Admin/Reports/Pos/AllDays/QrCodes/BranchWise/3-QrCodes/kLowDb/ReadFromFile/GroupByBranch/MaxAndMinFuncs/QrStartFunc.js
@@ -2,27 +2,24 @@ let StartFunc = ({ inGroupedData }) => {
     const grouped = inGroupedData;
 
     let LocalReturnArray = [];
-
-    for (const [key, value] of Object.entries(grouped)) {
-        LocalReturnArray.push({
-            BranchName: key,
-            QrCount: value.length,
-        });
-    };
-
-
     let LocalTodayQrCodes = [];
+
     const today = new Date().toISOString().split('T')[0];
 
     for (const [key, value] of Object.entries(grouped)) {
-        const todayQRCodes = value.filter(qr => new Date(qr.BookingData.OrderData.Currentdateandtime).toISOString().split('T')[0] === today);
+        if (key.startsWith("BranOrders")) continue;
 
-        if (todayQRCodes.length > 0) {
-            LocalTodayQrCodes.push({
-                BranchName: key,
-                QrCount: todayQRCodes.length,
-            });
-        }
+        LocalReturnArray.push({
+            BranchName: key,
+            QrCount: value ? value.length : 0,
+        });
+
+        const todayQRCodes = value ? value.filter(qr => new Date(qr.BookingData.OrderData.Currentdateandtime).toISOString().split('T')[0] === today) : [];
+
+        LocalTodayQrCodes.push({
+            BranchName: key,
+            QrCount: todayQRCodes.length,
+        });
     }
 
     return { AllQrCodes: LocalReturnArray, TodayQrCodes: LocalTodayQrCodes };
