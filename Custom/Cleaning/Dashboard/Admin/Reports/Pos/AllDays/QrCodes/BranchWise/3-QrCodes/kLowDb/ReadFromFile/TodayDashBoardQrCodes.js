95fd59e99360d82ebd87a7ed7f88a762807920ca
sharath0036 <ksharathbabu2@gmail.com>
2025-03-06 13:45:02 +0530

    end point bug branchName

--- Custom/Cleaning/Dashboard/Admin/Reports/Pos/AllDays/QrCodes/BranchWise/3-QrCodes/kLowDb/ReadFromFile/TodayDashBoardQrCodes.js
+++ Custom/Cleaning/Dashboard/Admin/Reports/Pos/AllDays/QrCodes/BranchWise/3-QrCodes/kLowDb/ReadFromFile/TodayDashBoardQrCodes.js
@@ -1,6 +1,9 @@
 import { StartFunc as buildData } from '../../../../CommonFuncs/buildData.js';
 
 let StartFunc = ({ inBranch }) => {
+    let CommonReplaceText = "BranOrders";
+    let LocalBranch = inBranch.replace(CommonReplaceText, "");
+
     const LocalQrCodeData = buildData();
     const today = new Date().toISOString().split('T')[0];
 
@@ -8,7 +11,7 @@ let StartFunc = ({ inBranch }) => {
 
         const elementDate = new Date(element.OrderDateTime).toISOString().split('T')[0];
         
-        return element.BranchName === inBranch && elementDate === today;
+        return element.BranchName === LocalBranch && elementDate === today;
     });
 
     return LocalFilteredData.slice().reverse();
